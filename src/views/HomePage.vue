98a2d65e5e6c218648165946d27a60864c758ebb
LuisMunar <luis.emunarr@gmail.com>
2023-02-17 16:43:04 -0500

    Add routes home and detail to pages

---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      Homa page
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonPage } from '@ionic/vue';
+</script>
+
+<style scoped>
+</style>
