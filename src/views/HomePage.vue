4bbb83fb391d6bc1fc80d18bc0de36cd649bae7d
LuisMunar <luis.emunarr@gmail.com>
2023-02-18 08:29:33 -0500

    Add fonts configurariont and scc class colors

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,16 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      Homa page
+      <p class="text-headline">Headline</p>
+      <p class="text-subtitle-one">subtitle</p>
+      <p class="text-subtitle-two">subtitle</p>
+      <br>
+      <p class="text-body-bold">subtitle</p>
+      <p class="text-body-medium">subtitle</p>
+      <p class="text-body-regular">subtitle</p>
+      <br>
+      <p class="text-caption-medium">subtitle</p>
+      <p class="text-caption-regular">subtitle</p>
     </ion-content>
   </ion-page>
 </template>
