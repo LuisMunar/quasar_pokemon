c372be7b6100afe0bf5603aabde62244b3ead4b1
LuisMunar <luis.emunarr@gmail.com>
2023-02-19 10:11:59 -0500

    Add paginator layout II

--- src/components/Home/HomePaginator/HomePaginatorStateless.vue
+++ src/components/Home/HomePaginator/HomePaginatorStateless.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="background-light p-2 d-flex d-align-items-center d-justify-content-space-between">
+  <div class="background-light p-2 d-flex d-align-items-center d-justify-content-center">
     <ion-button fill="clear">
       <ion-icon :icon="chevronBackOutline" size="large" color="primary"></ion-icon>
     </ion-button>
     <ion-button fill="clear">1</ion-button>
-    <ion-button fill="solid">2</ion-button>
-    <ion-button fill="clear">3</ion-button>
+    <ion-button fill="clear">2</ion-button>
+    <ion-button fill="solid">3</ion-button>
+    <ion-button fill="clear">4</ion-button>
+    <ion-button fill="clear">5</ion-button>
     <ion-button fill="clear">
       <ion-icon :icon="chevronForwardOutline" size="large" color="primary"></ion-icon>
     </ion-button>
