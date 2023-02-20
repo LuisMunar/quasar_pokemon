dc5351ff4a88d92a4e322b255133cd375eb613b4
LuisMunar <luis.emunarr@gmail.com>
2023-02-20 11:06:14 -0500

    Add layout to pokemon details abilities component

--- src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailAbilities/PokemonDetailAbilitiesStateless.vue
+++ src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailAbilities/PokemonDetailAbilitiesStateless.vue
@@ -1,11 +1,67 @@
 <template>
-  <div>
-    habilities segment
-  </div>
+  <ion-grid>
+    <ion-row class="mb-1">
+      <ion-col class="text-body-regular color-tertiary">Species</ion-col>
+      <ion-col class="text-body-medium color-dark">Seed</ion-col>
+    </ion-row>
+
+    <ion-row class="mb-1">
+      <ion-col class="text-body-regular color-tertiary">Height</ion-col>
+      <ion-col class="text-body-medium color-dark">2’3.”6 (0.70cm)</ion-col>
+    </ion-row>
+
+    <ion-row class="mb-1">
+      <ion-col class="text-body-regular color-tertiary">Weight</ion-col>
+      <ion-col class="text-body-medium color-dark">6.9kg (15.2lbs)</ion-col>
+    </ion-row>
+
+    <ion-row class="mb-1">
+      <ion-col class="text-body-regular color-tertiary">Abilities</ion-col>
+      <ion-col class="text-body-medium color-dark">Pending to know</ion-col>
+    </ion-row>
+
+    <ion-row class="mb-1">
+      <ion-col class="text-body-regular color-tertiary">Experience</ion-col>
+      <ion-col class="text-body-medium color-dark">24</ion-col>
+    </ion-row>
+
+    <ion-row class="mb-1">
+      <ion-col class="text-body-regular color-tertiary">Type</ion-col>
+      <ion-col class="text-body-medium color-dark d-flex d-align-items-center d-justify-content-space-between d-wrap">
+        <span class="color-light mr-1 mb-1 green pokemon-detail-abilities-chip">Grass</span>
+        <span class="color-light mr-1 mb-1 purple pokemon-detail-abilities-chip">Poison</span>
+        <span class="color-light mr-1 mb-1 green pokemon-detail-abilities-chip">Grass</span>
+        <span class="color-light mr-1 mb-1 purple pokemon-detail-abilities-chip">Poison</span>
+        <span class="color-light mr-1 mb-1 green pokemon-detail-abilities-chip">Grass</span>
+        <span class="color-light mr-1 mb-1 purple pokemon-detail-abilities-chip">Poison</span>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+
 </template>
 
 <script lang="ts">
+import { IonCol, IonGrid, IonRow } from '@ionic/vue'
+
 export default {
-  name: 'PokemonDetailAbilitiesStateless'
+  name: 'PokemonDetailAbilitiesStateless',
+
+  components: { IonCol, IonGrid, IonRow }
 }
 </script>
+
+<style scoped>
+.pokemon-detail-abilities-chip {
+  padding: 5px 10px;
+  font-size: 14px;
+  border-radius: 4px;
+}
+
+.green {
+  background-color: #16C172;
+}
+
+.purple {
+  background-color: #6E44FF;
+}
+</style>
--- src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailGraphics/PokemonDetailGraphicsStateless.vue
+++ src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailGraphics/PokemonDetailGraphicsStateless.vue
@@ -1,11 +1,56 @@
 <template>
-  <div>
-    graphics segment
-  </div>
+  <h1 class="text-subtitle-two">Base stats</h1>
+
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+    height="250"
+  />
 </template>
 
 <script lang="ts">
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale)
+ChartJS.defaults.font.size = 12
+ChartJS.defaults.font.family = 'Roboto-Medium'
+
 export default {
-  name: 'PokemonDetailGraphicsStateless'
+  name: 'PokemonDetailGraphicsStateless',
+
+  components: {
+    Bar
+  },
+
+  data: () => ({
+    chartData: {
+      labels: [ 'Speed', 'Sp Defense', 'Sp Attack', 'Defense', 'Attack', 'HP' ],
+      datasets: [
+        {
+          data: [40, 20, 12, 22, 31, 18],
+          borderColor: '#072AC8',
+          backgroundColor: '#072AC8',
+        }
+      ]
+    },
+    chartOptions: {
+      responsive: true,
+      indexAxis: 'y',
+      scales: {
+        x: {
+          ticks: {
+            color: '#8D91A5'
+          }
+        },
+        y: {
+          ticks: {
+            color: '#8D91A5'
+          }
+        }
+      }
+    }
+  })
 }
 </script>
