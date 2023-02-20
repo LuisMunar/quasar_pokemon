042fa45b373b9d21c675efb7e61bc57853055209
LuisMunar <luis.emunarr@gmail.com>
2023-02-20 11:18:59 -0500

    Add fix to pokemon detail tabs component II

--- src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailGraphics/PokemonDetailGraphicsStateful.vue
+++ src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailGraphics/PokemonDetailGraphicsStateful.vue
@@ -1,13 +1,48 @@
 <template>
-  <pokemon-detail-graphics-stateless />
+  <pokemon-detail-graphics-stateless :chart-options="chartOptions" :chart-data="chartData" />
 </template>
 
 <script lang="ts">
+import { Chart as ChartJS, Title, Tooltip, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
 import PokemonDetailGraphicsStateless from '@/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailGraphics/PokemonDetailGraphicsStateless.vue'
+
+ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale)
+ChartJS.defaults.font.size = 12
+ChartJS.defaults.font.family = 'Roboto-Medium'
 
 export default {
   name: 'PokemonDetailGraphicsStateful',
 
-  components: { PokemonDetailGraphicsStateless }
+  components: { PokemonDetailGraphicsStateless },
+
+  data: () => ({
+    chartOptions: {
+      responsive: true,
+      indexAxis: 'y',
+      scales: {
+        x: {
+          ticks: {
+            color: '#8D91A5'
+          }
+        },
+        y: {
+          ticks: {
+            color: '#8D91A5'
+          }
+        }
+      }
+    },
+    chartData: {
+      labels: [ 'Speed', 'Sp Defense', 'Sp Attack', 'Defense', 'Attack', 'HP' ],
+      datasets: [
+        {
+          data: [40, 20, 12, 22, 31, 18],
+          borderColor: '#072AC8',
+          backgroundColor: '#072AC8',
+        }
+      ]
+    }
+  })
 }
 </script>
--- src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailGraphics/PokemonDetailGraphicsStateless.vue
+++ src/components/Pokemon/PokemonDetail/PokemonDetailTabs/PokemonDetailGraphics/PokemonDetailGraphicsStateless.vue
@@ -11,11 +11,6 @@
 
 <script lang="ts">
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale)
-ChartJS.defaults.font.size = 12
-ChartJS.defaults.font.family = 'Roboto-Medium'
 
 export default {
   name: 'PokemonDetailGraphicsStateless',
@@ -24,33 +19,15 @@ export default {
     Bar
   },
 
-  data: () => ({
-    chartData: {
-      labels: [ 'Speed', 'Sp Defense', 'Sp Attack', 'Defense', 'Attack', 'HP' ],
-      datasets: [
-        {
-          data: [40, 20, 12, 22, 31, 18],
-          borderColor: '#072AC8',
-          backgroundColor: '#072AC8',
-        }
-      ]
-    },
+  props: {
     chartOptions: {
-      responsive: true,
-      indexAxis: 'y',
-      scales: {
-        x: {
-          ticks: {
-            color: '#8D91A5'
-          }
-        },
-        y: {
-          ticks: {
-            color: '#8D91A5'
-          }
-        }
-      }
+      type: Object,
+      required: true
+    },
+    chartData: {
+      type: Object,
+      required: true
     }
-  })
+  }
 }
 </script>
